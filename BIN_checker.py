112a88ddbf430e872d04cf18d90a5005976fef66
Azuremods <97257024+Azuremods@users.noreply.github.com>
2022-01-09 18:25:51 +0300

    Fix errors

--- BIN_checker.py
+++ BIN_checker.py
@@ -6,7 +6,7 @@ The script is made by Azuremods
 import os
 from colorama import *
 import requests
-
+import json
 token = ""
 
 
@@ -17,10 +17,10 @@ def check(token):
     print("Enter the card's BIN (first six digits)\n")
     bin_card = str(input("BIN: "))
     r = requests.get(url+bin_card,headers=headers)
-    result = r.content
+    result = r.text
     print(result)
     #try:
-    #    print("Bank:",result['bank_name'],"\nCountry:",result['country'],"\nSheme: ",result['sheme'],"\nType: ",result['type'],"\nUrl: ",result['url'])
+    #   print("Bank:",result['bank_name'],"\nCountry:",result['country'],"\nSheme: ",result['sheme'],"\nType: ",result['type'],"\nUrl: ",result['url'])
     #except:
     #    print(result["message"])
     
@@ -28,29 +28,29 @@ def check(token):
 def next_bins():
     print("Should I use the system token or yours? [s/y]\n")
     kil = str(input(">> "))
-    if "s" or "S" in kil:
+    if kil == "s":
         
         token = "7WRajfHIqNXXveImvJ6OhYSBNDxzv5Zu"
         os.system("clear")
         check(token)
-    elif "y" or "Y" in kil:
+    elif kil == "y":
         print("Enter your token from the site https://promptapi.com/account")
         
         token = str(input("Token: "))
         os.system("clear")
         check(token)
-
+    else: os._exit(200)
 def check_bins():
     os.system("figlet -f small Attention")
     print(Fore.RED + "The author of this script is not responsible for your actions\neverything you do is done at your own risk.\nContinue? [y/n]" + Style.RESET_ALL)
     repl_y_ = str(input(">> "))
-    if "y" or "Y" in repl_y_:
+    if  repl_y_ == "y":
         os.system("clear")
         next_bins()
-    elif "n" or "N" in repl_y_:
+    elif repl_y_ == "n":
         os.system("clear")
         menu_start()
-
+    else: os._exit(200)
 def menu_start():
     
     os.system("figlet -f small Bin Checker")
